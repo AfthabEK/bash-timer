9387f48c5256d0240977932b1aa5a374353df55e
AfthabEK <mohamedafthabek@gmail.com>
2025-02-07 18:57:52 +0530

    Added timer

--- timer.sh
+++ timer.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+	echo "Usage: $0 <minutes>"
+	exit 1
+fi
+
+minutes=$1
+
+duration=$((minutes * 60))
+
 start=$SECONDS
 
-while ((SECONDS-start < 5));
+while ((SECONDS-start < duration));
 do
 	:
 done
 
 
 ffplay -nodisp -autoexit klee.m4a
-echo "5SecondsLOL"
